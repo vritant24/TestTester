0f6e0aa8a3f85b70b5175e9a8edfd8907bafa3d3
vritant24 <vritoku@gmail.com>
2017-09-15 20:04:18 -0400

    log in works

--- MiddleMan/index.js
+++ MiddleMan/index.js
@@ -1,8 +1,48 @@
 var express = require('express');
 var app = express();
+var passport = require('passport')
+var githubStrategy = require('passport-github')
+
+app.use(require('serve-static')(__dirname + '/../../public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new githubStrategy({
+  clientID: "68bca50fa8ec6e0034e9",
+  clientSecret: '527ab44c8bf49732ff5abff6999718283d12a52b',
+  callbackURL: "http://localhost:8080/auth/github/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  cb(null, profile.id)
+}
+))
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', {failureRedirect: '/'}), 
+    function(req, res) {
+      res.redirect('/success');
+    }
+  );
 
 app.listen(8080);
