3a9a1161229b188e50743b9e8d6e3e77080c4ba9
jonahheeren <jonah.heeren@me.com>
2017-09-18 20:07:45 -0400

    GitHub Auth Using Passport & SQL Setup (#5)
    * added useless file
    
    * added passport and passport-github
    
    * log in works
    
    * store github auth stuff in obj
    
    * Linked React files with node
    
    * removed service worker
    
    * changed root route
    
    * added some comments
    
    * Added the SQL file, Atom style fixes
    
    * Remove node modules that were committed
    
    * Remove top level package.json
    
    * Remove dummy.md
    
    * DB Configured and Running

---
+++ db/db.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+var nconf = require('nconf');
+
+nconf.file({
+  file: '../config/config.json'
+});
+if (!Object.keys(nconf.get()).length) {
+  throw new Error('Unable to load config file. Check to make sure config/config.json exists');
+}
+
+var connection = mysql.createConnection(nconf.get('mysql'));
+
+exports.getUsers = () => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM User', function(error, results, fields) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
