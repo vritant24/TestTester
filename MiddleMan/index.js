bd44d22ade04997060ee01df95f5593e188822bb
vritant24 <vritoku@gmail.com>
2017-09-20 17:35:33 -0400

    added comments for shulin

--- MiddleMan/index.js
+++ MiddleMan/index.js
@@ -16,15 +16,27 @@ app.set('view engine', 'html');
 app.use(require('body-parser').urlencoded({ extended: true }));
 
 
-
 //=========== Routes for API ============
-
 app.get('/authenticate/:access_code/:session_id', function(req, res) {
   console.log(req.params.access_code) //access code for doing GitHub OAuth
   console.log(req.params.session_id)  //session id to keep track of user
-
+  obj = req.params
+  //return to client user name, user email
   res.send(JSON.stringify("Hola")) // temporary response
 }); 
+
+// /repository/:USessionId
+// return list of repositories
+
+// /repository/new/:USessionId/:name
+// create a new repo and return success / failure
+
+// /resetpassword/:USessionId/:oldPass/:newPass
+// return success/failure
+
+// /testlogs/:USessionId/:RepoName
+// return list of test logs
+
 
 app.listen(8080);
 
