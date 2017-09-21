dbea62cf9b8687912be1d1cfc3f0d7ad0f6c9f3d
“Shulin <ernestwoods0516@gmail.com>
2017-09-20 22:48:13 -0400

    finish four functions, wait for checking

--- MiddleMan/index.js
+++ MiddleMan/index.js
@@ -2,30 +2,15 @@ var express     = require('express');
 var engines     = require('consolidate');
 //var db          = require('../db/db.js');
 var utils       = require('./utils.js')
-/*
-var obj = {
-	github: ' '
-}
-*/
 
-// var obj = [];
-// var len = 10;
-// for (var i = 0; i < len; i++) {
-//     obj.push({
-//         github: ' '
-//     });
-// }
 var obj = {}
 
 var app         = express();
 
 //dummy variables for now:
 
-var repo_x = {
-	"type": "git",
-    "url": "git+https://github.com/repo_x"
-}
-
+var repoList = ["x","y","z","a","b","c"];
+var testLogList = ["test1","test2","test3","test4"];
 
 //Serve react and static files
 app.use(express.static(__dirname + '/../client/build'));
@@ -44,11 +29,8 @@ app.get('/authenticate/:access_code/:session_id', function(req, res) {
   console.log(req.params.session_id)  //session id to keep track of user
   obj[req.params.session_id] = {}
   obj[req.params.session_id].github = req.params.access_code
-  //obj.github = req.params.session_id;
-  console.log(obj[req.params.session_id].github);
   
-  //obj = req.params
-  //return to client user name, user email
+  
   res.send(JSON.stringify("Hola")) // temporary response
 }); 
 
@@ -56,18 +38,14 @@ app.get('/authenticate/:access_code/:session_id', function(req, res) {
 // return list of repositories
 
 app.get('/repository/:USessionId', function(req, res) {
-  console.log(req.params.access_code) //access code for doing GitHub OAuth
-  console.log(req.params.session_id)  //session id to keep track of user
-  //obj[req.params.session_id].github = req.params.access_code
-  /*
-  bj = req.params
-  if (obj.github == req.params.USessionId){
-  	  res.send(JSON.stringify(repo_x))
+
+  if (obj[req.params.USessionId]){
+        res.send(JSON.stringify(repoList));
+    }
+  else {
+        res.send(JSON.stringify("User ID doesn't exist"));
   }
-  */
-  //console.log(repo_x);
-  //return to client user name, user email
-  //res.send(JSON.stringify(repo_x)) // temporary response
+ 
 }); 
 
 
@@ -75,12 +53,62 @@ app.get('/repository/:USessionId', function(req, res) {
 // /repository/new/:USessionId/:name
 // create a new repo and return success / failure
 
+app.get('/repository/new/:USessionId/:name', function(req, res) {
+
+  repoList.push(req.params.name);
+  console.log(repoList);
+
+  if (repoList.includes(req.params.name)){
+      res.send(JSON.stringify("Success"));
+      
+  }
+  else {
+      res.send(JSON.stringify("Failure"));
+
+  }
+
+}); 
+
+
 // /resetpassword/:USessionId/:oldPass/:newPass
 // return success/failure
 
+app.get('/resetpassword/:USessionId/:oldPass/:newPass', function(req, res) {
+    obj[req.params.USessionId] = {}
+    obj[req.params.USessionId].password = "oldP" //Assume user has a password, a temp variable.
+    if (obj[req.params.USessionId].password == req.params.oldPass){
+        obj[req.params.USessionId].password = req.params.newPass;
+    } 
+    console.log(req.params.newPass);
+    if (obj[req.params.USessionId].password == req.params.newPass){
+        res.send(JSON.stringify("Success"));
+      
+    }
+    else {
+        res.send(JSON.stringify("Failure"));
+
+    }
+
+}); 
+
+
 // /testlogs/:USessionId/:RepoName
 // return list of test logs
-
+app.get('/testlogs/:USessionId/:RepoName', function(req, res) {
+  //need to run the first get() function to make sure the user exist
+  if (obj[req.params.USessionId]){
+        if (repoList.includes(req.params.RepoName)){
+            res.send(JSON.stringify(testLogList));
+        }
+        else{
+            res.send(JSON.stringify("Repository doesn't exist"));
+        }
+    }
+  else {
+        res.send(JSON.stringify("User ID doesn't exist"));
+  }
+ 
+}); 
 
 app.listen(8080);
 
