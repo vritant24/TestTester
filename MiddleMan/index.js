68bc28f3aa4019a8ecd3bf155f89bacee482cf31
Brandon <brandonmarx123@gmail.com>
2017-09-26 13:08:27 -0400

    basic changes

--- MiddleMan/index.js
+++ MiddleMan/index.js
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
 var express = require('express');
 var app = express();
 var engines = require('consolidate');
 var request = require('request');
-//var db = require('../db/db.js');
+//var db = require('../HWdb/db.js');
 var utils = require('./utils.js')
 var obj = {}
 
 //Dev values
 var CLIENT_ID     = '2a48dc27e13bf25eca10';
 var CLIENT_SECRET = 'a3340700567cad703e00952f4b740d065c1b297d';
-=======
-var express     = require('express');
-var engines     = require('consolidate');
-//var db          = require('../db/db.js');
-var utils       = require('./utils.js')
-
-var obj = {}
-
-var app         = express();
-
-//dummy variables for now:
-
-var repoList = ["x","y","z","a","b","c"];
-var testLogList = ["test1","test2","test3","test4"];
->>>>>>> master
 
 //Serve react and static files
 app.use(express.static(__dirname + '/../client/build'));
@@ -36,7 +20,6 @@ app.set('view engine', 'html');
 app.use(require('body-parser').urlencoded({ extended: true }));
 
 
-<<<<<<< HEAD
 
 //=========== Routes for API ============
 var accessCode;
@@ -61,103 +44,15 @@ app.get('/authenticate/:access_code/:session_id', function(req, res) {
   res.send(JSON.stringify("Hola")) // temporary response
 
 
-=======
-//=========== Routes for API ============
-
-app.get('/authenticate/:access_code/:session_id', function(req, res) {
-  console.log(req.params.access_code) //access code for doing GitHub OAuth
-  console.log(req.params.session_id)  //session id to keep track of user
-  obj[req.params.session_id] = {}
-  obj[req.params.session_id].github = req.params.access_code
-  
-  
-  res.send(JSON.stringify("Hola")) // temporary response
-}); 
-
-// /repository/:USessionId
-// return list of repositories
-
-app.get('/repository/:USessionId', function(req, res) {
-
-  if (obj[req.params.USessionId]){
-        res.send(JSON.stringify(repoList));
-    }
-  else {
-        res.send(JSON.stringify("User ID doesn't exist"));
-  }
- 
->>>>>>> master
 }); 
 
 
 
-<<<<<<< HEAD
 
 
 
 
 
-=======
-// /repository/new/:USessionId/:name
-// create a new repo and return success / failure
-
-app.get('/repository/new/:USessionId/:name', function(req, res) {
-
-  repoList.push(req.params.name);
-  console.log(repoList);
-
-  if (repoList.includes(req.params.name)){
-      res.send(JSON.stringify("Success"));
-      
-  }
-  else {
-      res.send(JSON.stringify("Failure"));
-
-  }
-
-}); 
-
-
-// /resetpassword/:USessionId/:oldPass/:newPass
-// return success/failure
-
-app.get('/resetpassword/:USessionId/:oldPass/:newPass', function(req, res) {
-    obj[req.params.USessionId] = {}
-    obj[req.params.USessionId].password = "oldP" //Assume user has a password, a temp variable.
-    if (obj[req.params.USessionId].password == req.params.oldPass){
-        obj[req.params.USessionId].password = req.params.newPass;
-    } 
-    console.log(req.params.newPass);
-    if (obj[req.params.USessionId].password == req.params.newPass){
-        res.send(JSON.stringify("Success"));
-      
-    }
-    else {
-        res.send(JSON.stringify("Failure"));
-
-    }
-
-}); 
-
-
-// /testlogs/:USessionId/:RepoName
-// return list of test logs
-app.get('/testlogs/:USessionId/:RepoName', function(req, res) {
-  //need to run the first get() function to make sure the user exist
-  if (obj[req.params.USessionId]){
-        if (repoList.includes(req.params.RepoName)){
-            res.send(JSON.stringify(testLogList));
-        }
-        else{
-            res.send(JSON.stringify("Repository doesn't exist"));
-        }
-    }
-  else {
-        res.send(JSON.stringify("User ID doesn't exist"));
-  }
- 
-}); 
->>>>>>> master
 
 app.listen(8080);
 
--- client/src/router.js
+++ client/src/router.js
@@ -3,45 +3,22 @@ import ReactDOM                 from 'react-dom'        // show react elements
 import Router                   from 'ampersand-router' // internal navigation
 import qs                       from 'qs'               // create queries
 import uuid                     from 'uuid'             // generate random string
-<<<<<<< HEAD
 import xhr                      from 'xhr'              // http request with server
 import { Repos, User }   from './pages'
 // import app              from 'ampersand-app'
-=======
-import app                      from 'ampersand-app'
-     
-import { session }              from './helpers'
-import { Repos, User, Landing } from './pages' 
->>>>>>> master
 
 export default Router.extend({
     // the routes
     routes: {
-<<<<<<< HEAD
         ''              : 'login',
         'logout'        : 'logout',
         'repos'  : 'repos',
         'user'   : 'user',
 
-=======
-        ''       : 'landing',
-        'login'  : 'login',
-        'logout' : 'logout',
-        'repos'  : 'repos',
-        'user'   : 'user',
-         
->>>>>>> master
         'auth/callback?:query' : 'authCallback',
     },
 
     // functions called for each route
-<<<<<<< HEAD
-=======
-    landing() {
-        renderPage(<Landing/>)
-    },
-
->>>>>>> master
     login () {
         const state = uuid();
         window.localStorage.state = state;
@@ -52,28 +29,14 @@ export default Router.extend({
             state           : state,  
         })
     }, 
-<<<<<<< HEAD
     logout () {
        
     },
-=======
-
-    logout () {
-       window.localStorage.clear()
-
-       // send something to server
-
-       // redirect to root route
-       window.location = '/'
-    },
-
->>>>>>> master
     authCallback (query) {
         //get code and state from query 
         query = qs.parse(query)
 
         if(query.state === window.localStorage.state) {
-<<<<<<< HEAD
             //send code to server along with a session id to be redirected to dashboard with userid
             var state = uuid() // session id
             window.localStorage.sate = state;
@@ -89,35 +52,11 @@ export default Router.extend({
     repos () {
         renderPage(<Repos/>)
     },
-=======
-            window.localStorage.state = null; //remove state
-
-            //send code to server along with a session id to be redirected to dashboard with userid
-            var sessionID = session.setSessionID()
-
-            fetch('/authenticate/' + query.code + '/' + sessionID)  // '/authenticate/:access_code/:session_id'
-            .then(res => res.json())                                //receive response and convert to JSON
-            .then(res => {
-                // redirect to repos
-                this.redirectTo('/repos')
-            });
-
-        } else {
-            window.localStorage.state = null; //remove state
-            console.log("uh oh")
-        }
-    },
-
-    repos () {
-        renderPage(<Repos/>)
-    },
-
->>>>>>> master
     user () {
         renderPage(<User/>)
     },
  })
-
+ 
  //Render's the given page using the react dom
  var renderPage = (component) => {
     ReactDOM.render(component, document.getElementById('root')
