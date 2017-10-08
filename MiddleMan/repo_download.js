2b05710f279eeef0dc395454a878397dd2146292
Brandon <brandonmarx123@gmail.com>
2017-10-08 19:40:51 -0400

    started building PROPER file downloading

--- MiddleMan/repo_download.js
+++ MiddleMan/repo_download.js
@@ -1,7 +1,18 @@
 var ghdownload = require('github-download')
+var curl = require('curlrequest')
 
 
-export function downloadRepo(url){
+export function downloadRepo(userID, repoName){
+
+    //curl https://api.github.com/repos/USER_NAME/REPO_NAME/tarball?access_token=ACESS_TOKEN
+    //file structure: users/USER_NAME/REPO_NAME/main <--- code goes here
+    var USER_NAME = userid
+    var REPO_NAME = repoName
+    var ACCESS_TOKEN = ""; //FIGURE OUT HOW TO QUERY THE TOKEN
+
+
+    var URLString = "https://api.github.com/repos/" + USER_NAME + "/" + REPO_NAME + "/tarball?access_token=" + ACCESS_TOKEN
+
 
     ghdownload(url)
 
