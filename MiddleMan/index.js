40df502e2c75ba76fe6e9b3cb62afdb16b11cac9
jonahheeren <jonah.heeren@me.com>
2017-10-19 18:50:30 -0400

    Move to the webhook spot

--- MiddleMan/index.js
+++ MiddleMan/index.js
@@ -91,20 +91,6 @@ app.get('/repos/:session_id', function(req, res) {
 app.get('/monitor/:session_id/:repo_id', function(req, res) {
     //TODO catch error and send status code
 
-    db.getUserAccessFromSession(req.params.session_id).then(function(user_access_row) {
-        var user_access = user_access_row[0];
-        db.getRepoURL(req.params.repo_id).then(function(repo_rows) {
-            var repo = repo_rows[0];
-            github.getRepoDownload(user_access.gitHubId, repo.repoURL, req.params.repo_id, user_access.accessToken).then(function() {
-                run_tests.unzipAndStore(user_access.gitHubId, req.params.repo_id).then(function() {
-                    run_tests.runTestScript(user_access.gitHubId, req.params.repo_id).then(function() {
-                        run_tests.parseScripts(user_access.gitHubId, req.params.repo_id);
-                    })
-                })
-            });
-        });
-    });
-
     db.monitorUserRepo(req.params.repo_id);
     var ret = {
         status: 200,
