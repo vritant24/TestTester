fe44e79ad2279b23f7f57ca4cc4cc6e29dd3ebbc
vritant24 <vritoku@gmail.com>
2017-10-20 09:13:27 -0400

    stashing

---
+++ tests/api-test.js
@@ -0,0 +1,9 @@
+let chai        = require('chai');
+let chaiHttp    = require('chai-http');
+
+let should = chai.should();
+chai.use(chaiHttp);
+
+descriibe('test db', function() {
+    it('Should return user data')
+});
