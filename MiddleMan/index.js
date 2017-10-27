e2318a67c82e0942aa1e29972856d507ead17219
jonahheeren <jonah.heeren@me.com>
2017-10-27 11:25:49 -0400

    Don't use completely static value

--- MiddleMan/index.js
+++ MiddleMan/index.js
@@ -141,7 +141,7 @@ app.get('/monitor/:session_id/:repo_id', function(req, res) {
                                          utils.deployProd(id, repo_id, report)])
                             // Promise.all([utils.deployAlpha(user_access.gitHubId, req.params.repo_id, report)])    
                             .then(values => {
-                                values[2] = 10001;
+                                values[2] = values[2] + 1;
                                 console.log(values);
                                 values.forEach(function(port) {
                                     db.addRepoDeployment([repo_id, port])
